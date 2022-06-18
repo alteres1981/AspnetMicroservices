6ae455d8767d543b0753231c397003aac86a8b66
alteres1981 <alteres@gmail.com>
2022-06-18 12:04:59 +0200

    Compila y ejecuta OK.

--- src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -8,9 +8,9 @@ namespace Catalog.API.Data
         public CatalogContext(IConfiguration configuration)
         { 
             var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
-            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:Database"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
 
-            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:Collection"));
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
             CatalogContextSeed.SeedData(Products);
         }
 
